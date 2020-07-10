768192d6621c0f1e03db8e8971557f96cc5d96b2
oladimeji-ojo <ojokuredim@gmail.com>
2020-07-10 12:19:52 +0100

    simple unit test for normalizeInput func

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,1 +1,17 @@
+const axios = require("axios");
+const {
+  getWeatherInfo,
+  normalizeInput,
+  standardizeLocalTime,
+} = require("../helpers");
 
+test("normalizeInput works as it should", () => {
+  expect(
+    normalizeInput([" utah", "Ikeja ", "       Tokyo", "  Paris", " Lagos"])
+  ).toEqual(["utah", "Ikeja", "Tokyo", "Paris", "Lagos"]);
+  expect(normalizeInput(["Accra ", "Lagos", "  Geneva"])).not.toEqual([
+    "Accra ",
+    "Lagos",
+    "  Geneva",
+  ]);
+});
